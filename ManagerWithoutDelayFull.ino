1e5a1827baefdded9fef61c9c76caa7a94befd01
l.burchuladze <l.burchuladze@rowi.com>
2023-04-07 15:08:19 +0400

    обновил

--- ManagerWithoutDelayFull.ino
+++ ManagerWithoutDelayFull.ino
@@ -2,17 +2,17 @@ int pins[] = {3, 5, 6, 9, 10, 11};
 int LenPins = sizeof(pins);
 int states[] = {LOW, LOW, LOW, LOW, LOW, LOW};
 unsigned long prevMillis[] = {0, 0, 0, 0, 0, 0};
-long intervals[] = {60, 900, 1200, 15000, 80000, 210000};
+long intervals[] = {10, 1000, 2000, 3000, 4000, 5000};
 
-unsigned long millis;
+unsigned long crnMillis;
 
 void loop() {
-  millis = micros();
+  crnMillis = micros();
   for (int i = 0; i < LenPins; i++)
 	{    
-    if (millis - prevMillis[i] >= intervals[i]) 
+    if (crnMillis - prevMillis[i] >= intervals[i]) 
 	{
-      prevMillis[i] = millis;
+      prevMillis[i] = crnMillis;
   
     if (states[i] == LOW) 
 	{
